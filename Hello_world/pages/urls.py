5c00a0c56992c972e5aeff07e20821f8400fa706
Rafay <kuoongfu@gmail.com>
2024-09-24 18:51:05 +0500

    Added some stuff

--- Hello_world/pages/urls.py
+++ Hello_world/pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from pages.views import homeView, aboutView, contactView, cartView
+from pages.views import homeView, aboutView, contactView, cartView, HomeView
 
 urlpatterns = [
-    path('home/', homeView.as_view()),
+    path('home/', HomeView.as_view()),
     path('about/', aboutView),
     path('contact/', contactView),
     path('cart/', cartView)
